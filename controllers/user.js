4a7092f1a43b10df48af1a0f9c5239b1e9766289
vincent <paintart.lin@gmail.com>
2018-10-06 15:57:04 +0800

    get user by class function added

--- controllers/user.js
+++ controllers/user.js
@@ -17,13 +17,16 @@ exports.getUsersByYear = async function (ctx, year) {
 	let limit = setting.limit;
 	let skip = (page - 1) * limit;
 	let re = new RegExp('^'+year);
-	console.log('re:', re);
 	ctx.body = await User.find({ account: { $regex: re } }).sort({ rank: 1 })
 	.skip(skip).limit(limit).select({ _id: false, __v: false });
 }
 
 exports.getUsersByClass = async function (ctx, cla) {
-	ctx.body = `get user by class ${cla}`;
+	let page = this.query.page || 1;
+	let limit = setting.limit;
+	let skip = (page - 1) * limit;
+	ctx.body = await User.find({ class: cla }).sort({rank: 1}).skip(skip).limit(limit)
+	.select({ _id: false, __v: false });
 }
 
 exports.update = async function (ctx) {
