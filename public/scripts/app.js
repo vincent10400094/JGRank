8dc1e92e7d8eecfbed62c3cf250db4d5db898349
vincent <paintart.lin@gmail.com>
2018-10-07 22:14:24 +0800

    add jquery && function handling data in app.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,9 +1,23 @@
 'use strict';
 
-fetch('/api/users?page=1')
-.then(response => response.text())
+const URL = 'https://judgegirl.csie.org/';
+
+const limit = 50;
+var page = 2;
+
+fetch(`/api/users/year/B07?page=${page}`)
+.then(response => response.json())
 .then(data => {
-	console.log(data);
+	data.map((data, i) => {
+		console.log(data);
+		let new_row = 	`<tr>
+							<td>${limit*(page-1)+i+1}</td>
+							<td><a href="${URL+data.url}">${data.account}</a></td>
+							<td>${data.AC}</td>
+							<td>${data.class}</td>
+						</tr>`;
+		$('tbody').append(new_row);
+	});
 }).catch(err => {
 	console.log('fetch failed:', err);
-})
+});
